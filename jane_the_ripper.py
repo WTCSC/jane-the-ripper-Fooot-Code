a793cb73c1f5e2e1d36b75a4f121a9c81e99157b
Fooot-Code <brennon.hill@gmail.com>
2025-11-03 09:40:21 -0700

    Needs more, i forgot what I did.

--- jane_the_ripper.py
+++ jane_the_ripper.py
@@ -1,35 +1,47 @@
 import hashlib
+import os
 
+def crackPasswords(hashFilePath: str, wordlistPath: str, algorithmToUse: tuple):
+    """
+    Takes 2 file paths, one for hashes, one for word lists, and an algorithm's
+    name and function in a tuple. It then hashes the words in the word file using
+    the specified algorithm, and then compares said hashes to the hash file given
+    to determine any equivalent hashes. It then prints out the hashes and their 
+    respective passwords.
+    """
 
-def crackPasswords(hashFilePath, wordlistPath, algorithmToUse):
-    
-    targetHashes = set()
-    uncrackableHashes = set()
+    targetHashes = set() # hashes from hashFilePath
+    uncrackableHashes = set() # hashes that weren't able to be cracked.
 
     # shake_* algorithms require an explicit digest length
     explicitDigestLength = ""
     if algorithmToUse[0].startswith("shake_"):
         explicitDigestLength = input("Since any shake algorithm requires an explicit digest length, what would you like that to be?\n> ")
 
+    # Adds hashes to the set of target hashes
     with open(hashFilePath, "r") as hashesFile:
         for hash in hashesFile:
             targetHashes.add(hash.strip())
 
+    # Cracks passwords by comparing word hashes with target hashes
     with open(wordlistPath, "r") as wordListFile:
         for word in wordListFile:
             wordEncodedStripped = word.strip().encode()
             hashObject = algorithmToUse[1](wordEncodedStripped) # [1] is the function, [0] is the string
-
             
+            # Calls a different function with a different argument for shake algorithms
             if explicitDigestLength:
                 hashedWord = hashObject.hexdigest(int(explicitDigestLength))
             else:
                 hashedWord = hashObject.hexdigest()
 
+            # If hash is cracked, then print the cracked hash
             if hashedWord in targetHashes:
                 print(f"[+] Cracked {hashedWord} --> {word}")
-            else:
-                uncrackableHashes.add(hashedWord)
+            else: # if not, add the hash to the uncrackable list
+                uncrackableHashes.add(targetHashes)
+    
+    # Prints unfound hashes
     print("Cracking is finished, here are hashes that weren't found:")
     for uncrackedHash in uncrackableHashes:
         print(f"[-] {uncrackedHash}")
@@ -37,6 +49,7 @@ def crackPasswords(hashFilePath, wordlistPath, algorithmToUse):
     
 
 def main():
+    # Hashing algorithms and their functions
     availableHashingAlgorithms = {
         'md5': hashlib.md5,
         'sha1': hashlib.sha1,
@@ -54,20 +67,31 @@ def main():
     tryAgain = True
 
     while tryAgain:
-        # hashFilePath = input("Please input your hash file path\n> ")
-        # wordlistFilePath = input("Please input your word list file path\n> ")
+        # Path for hash file
+        hashFilePath = input("Please input your hash file path\n> ")
+        while not os.path.exists(hashFilePath):
+            print("Please enter a valid path")
+            hashFilePath = input("> ")
+
+        # Path for word list
+        wordlistFilePath = input("Please input your word list file path\n> ")
+        while not os.path.exists(wordlistFilePath):
+            print("Please enter a valid path")
+            wordlistFilePath = input("> ")
+
+        # Type of hash algorithm, given in the dictionary above
         hashAlgorithmType = input("Please enter the type of hash algorithm you would like to use?\n> ")
         while hashAlgorithmType not in availableHashingAlgorithms.keys():
             print("Please select from this available list:")
             print(list(availableHashingAlgorithms.keys())) # prints this, as it is a cleaner set than the one I made
             hashAlgorithmType = input("> ")
 
-        algorithmToUse = (hashAlgorithmType, availableHashingAlgorithms[hashAlgorithmType]) # string of algorithm and function of algorithm
+        algorithmToUse = (hashAlgorithmType, availableHashingAlgorithms[hashAlgorithmType]) # string of algorithm type and function of algorithm
 
-        crackPasswords("hashes.txt", "wordlist.txt", algorithmToUse)
+        crackPasswords(hashFilePath, wordlistFilePath, algorithmToUse)
 
         print("Would you like to try again?")
-        tryAgain = False if input("(y or n)> ").lower() == "n" else True
+        tryAgain = True if input("(y or n)> ").lower() == "y" else False
     
 if __name__ == "__main__":
     main()
